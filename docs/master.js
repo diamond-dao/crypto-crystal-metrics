1e96c83b9b038a687123f7059d4f444fd54d5ea3
rohanbansal12 <rohan2003bansal@gmail.com>
2021-08-09 23:32:13 -0500

    Update

--- docs/master.js
+++ docs/master.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
   $("a").click(function () {
     $(this).attr("target", "_blank");
   });
+
+  $("#tab_metrics").click(function () {
+    var current_selected_tab = $("section.is-active");
+    $("#metrics-spinner").hide();
+  });
+
+  $(".toggle_words").click(function () {
+    console.log($(this).siblings(".top_words").text());
+    $(this).siblings(".top_words").toggle(500);
+    $(this).siblings(".least_words").toggle(500);
+  });
 });
